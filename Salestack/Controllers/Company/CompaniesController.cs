57fe653f7b569d3315d24976da61159ea0cf6ce4
JrSEngineer <jr.s.engineer@gmail.com>
2024-10-21 19:24:09 -0300

    :construction: Exposing methods to create/retrieve company  and director. :construction:

---
+++ Salestack/Controllers/Company/CompaniesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Salestack.Data.Context;
+using Salestack.Entities.Company;
+
+namespace Salestack.Controllers.Company;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CompaniesController : ControllerBase
+{
+    private readonly SalestackDbContext _context;
+
+    public CompaniesController(SalestackDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCompanyAsync(SalestackCompany data)
+    {
+        var newCompany = new SalestackCompany
+        {
+            Id = Guid.NewGuid(),
+            Name = data.Name,
+            Cnpj = data.Cnpj,
+            CompanyCode = data.CompanyCode
+        };
+
+        await _context.Company.AddAsync(newCompany);
+
+        await _context.SaveChangesAsync();
+
+        return Created("salestack-api", newCompany);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCompaniesAsync()
+    {
+        var companies = await _context.Company.ToListAsync();
+
+        return Ok(companies);
+    }
+}
---
+++ Salestack/Controllers/Users/Director/DirectorsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Salestack.Data.Context;
+using Salestack.Entities.Users;
+
+namespace Salestack.Controllers.Users.Director;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DirectorsController : ControllerBase
+{
+    private readonly SalestackDbContext _context;
+
+    public DirectorsController(SalestackDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateDirectorAsync(SalestackDirector data)
+    {
+        var newDirector = new SalestackDirector(
+            Guid.NewGuid(),
+            data.Name,
+            data.Email,
+            data.PhoneNumber,
+            data.Occupation);
+
+        await _context.Director.AddAsync(newDirector);
+
+        await _context.SaveChangesAsync();
+
+        return Created("salestack-api", newDirector);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllDirectorsAsync()
+    {
+        var directors = await _context.Director.ToListAsync();
+
+        return Ok(directors);
+    }
+}
