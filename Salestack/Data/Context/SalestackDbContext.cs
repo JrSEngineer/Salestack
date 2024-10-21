da0c684afe96ba22d609f4e09427a902dc2ce123
JrSEngineer <jr.s.engineer@gmail.com>
2024-10-21 19:20:59 -0300

    :building_construction: Adding DbContext to provide database interactions for application.

---
+++ Salestack/Data/Context/SalestackDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Salestack.Entities.Company;
+using Salestack.Entities.Users;
+
+namespace Salestack.Data.Context
+{
+    public class SalestackDbContext : DbContext
+    {
+        public SalestackDbContext(DbContextOptions<SalestackDbContext> options) : base(options)
+        {
+            bool pendingMigrationsFound = this.Database.GetPendingMigrations().Any();
+
+            if (pendingMigrationsFound) this.Database.Migrate();
+        }
+
+        public DbSet<SalestackCompany> Company { get; set; }
+        public DbSet<SalestackSeller> Seller {  get; set; }
+        public DbSet<SalestackManager> Manager {  get; set; }
+        public DbSet<SalestackDirector> Director {  get; set; }
+    }
+}
